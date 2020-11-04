88b7c32b5e570faf86263f120c36a7cec95933e2
root <yuyue_qty@163.com>
2020-11-05 00:02:10 +0800

    pom douyin

--- src/main/java/top/longmarch/douyin/request/TiaoYueAuthDouyinRequest.java
+++ src/main/java/top/longmarch/douyin/request/TiaoYueAuthDouyinRequest.java
@@ -21,7 +21,7 @@ public class TiaoYueAuthDouyinRequest extends AuthDouyinRequest {
                 .queryParam("response_type", "code")
                 .queryParam("client_key", config.getClientId())
                 .queryParam("redirect_uri", config.getRedirectUri())
-                .queryParam("scope", "user_info,video.create,video.delete,video.data,video.list,following.list,fans.list,hotsearch,fans.data,data.external.user,data.external.item,star_top_score_display,star_tops,star_author_score_display")
+                .queryParam("scope", "user_info,video.create,video.delete,video.data,video.list,video.search,following.list,fans.list,hotsearch,fans.data,data.external.user,data.external.item,star_top_score_display,star_tops,star_author_score_display")
                 .queryParam("state", getRealState(state))
                 .build();
     }
